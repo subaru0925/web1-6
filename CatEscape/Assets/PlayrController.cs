2a7eb0013b43ad3d0ad42b6432eddb2ab0e3ecd6
Unknown <adgjmptw.925@icloud.com>
2018-06-04 18:19:53 +0900

    コミット

---
+++ CatEscape/Assets/PlayrController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayrController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+    public void RButtonown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)){
+            transform.Translate(3, 0, 0);
+        }
+
+
+	}
+}
